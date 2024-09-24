3a90226c56bde52079930b51f554190e872a0d2b
TiDz <67941515+tidzvl@users.noreply.github.com>
2024-09-24 08:28:05 +0700

    Upload first time
    All dot file and main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include "include/list/listheader.h"
+// #include "include/list/XArrayListDemo.h"
+#include "include/list/DLinkedListDemo.h"
+
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    dlistDemo6();
+    return 0;
+}
